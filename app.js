158592580a7e1a4b790ec68f2cc0aa4fa2ccdebc
hector-jimenez <hector.jimenez@tektweak.com>
2017-09-01 22:14:30 -0500

    angular.js start files

---
+++ app.js
@@ -0,0 +1,8 @@
+(function () {
+  'use strict'; // best practise
+
+  angular.module('testApp', []) // brackets are dependencies, this bounds the app to the View
+    .controller('Controller1', function () { // this bounds the controller's function to the div in the view
+
+    })
+} )();
