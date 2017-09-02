39a47f97af3e343928b7124ce7813f41914cac4f
hector-jimenez <hector.jimenez@tektweak.com>
2017-09-01 23:38:36 -0500

    IOC example code update

--- iocApp.js
+++ iocApp.js
@@ -2,9 +2,18 @@
   'use strict'; // best practise
 
   angular.module('IocApp', []) // brackets are dependencies, this bounds the app to the View
-    .controller('IocController', IocController); // we define the name of the function for the controller
 
-    function IocController($scope, $filter) { // this is the controller's main function, the $filter is a service that lets us create format function to display html
+  // we can the name of the function for the controller as 2nd parameter (as an array)
+  // the last element is the actual function name
+  // the 1st and 2nd elements in the array indicate that the function uses 2 parameters and in the order specified,
+  // this is for minification of JS code, it would look like this:
+  //      .controller('IocController', ['$scope','$filter',IocController]);
+ // but we can also do this, more elegant:
+ .controller('IocController', IocController);
+ IocController.$inject = ['$scope','$filter'];
+
+ // and now define the controller's function
+ function IocController($scope, $filter) { // this is the controller's main function, the $filter is a service that lets us create format function to display html
       $scope.name = "Hector"
       $scope.upper = function () { // the $filter needs to be within the scope, as a function in this case called upper
           var upCase = $filter('uppercase'); // upCase is the name of the function for the $filter service
