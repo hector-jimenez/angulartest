503b09ca739c12c83251be7168d0be6036154864
hector-jimenez <hector.jimenez@tektweak.com>
2017-09-02 00:05:19 -0500

    angular js expression tests update

--- exprApp.js
+++ exprApp.js
@@ -7,8 +7,18 @@
 
     function ExprController($scope) {
       $scope.name = "Hector";
+      $scope.imageSelector = "one";
       $scope.sayMessage = function () {
           return "Focus man..";
       };
+
+      $scope.testButtonAction = function () {
+          if ($scope.imageSelector = "one") {
+            $scope.imageSelector = "two";
+          } else {
+            $scope.imageSelector = "one";
+          }
+      };
+
     }
 } )();
